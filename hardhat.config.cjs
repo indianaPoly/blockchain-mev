07c67aa25c9d9794a5b9b53a064942274d22ea85
indianaPoly <hyeonlimgo5@gmail.com>
2024-08-23 17:18:22 +0900

    style: cjs 변경

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -8,10 +8,5 @@ module.exports = {
     paths: {
         sources: './cointracts',
     },
-    networks: {
-        sepolia: {
-            url: `https://sepolia.infura/io/v3/${process.env.INFURA_API_KEY}`,
-            accounts: [`0x${process.env.PRIVATE_KEY}`],
-        },
-    },
+    networks: {},
 };
