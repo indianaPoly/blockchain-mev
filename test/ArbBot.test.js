dbcbc52edb1f0313b36b185c23fd239f0e7c11e0
indianaPoly <hyunrim03@naver.com>
2024-09-02 13:47:47 +0900

    test: flash loan 테스트 코드 작성

--- test/ArbBot.test.js
+++ test/ArbBot.test.js
@@ -2,6 +2,28 @@
 
 import hre from 'hardhat';
 
-const main = async () => {
-    const [deployer] = await hre.ethers.getSigners();
-};
+describe('arbBot contract depoly and test', async () => {
+  // Mock Token address or mainCurrency address
+  const mainCurrency = "";
+  let Arbot, arbBot;
+  let owner;
+
+  beforeEach(async () => {
+    [owner] = await hre.ethers.getSigners();
+
+
+    Arbot = hre.ethers.getContractFactory('ArbBot');
+
+    // 배포시에 owner address와 체인의 메인 토큰의 address가 들어감
+    arbBot = await Arbot.deploy(owner.address, mainCurrency);
+    await arbBot.waitForDeployment();
+
+    console.log("sucessful initialize!");
+  });
+
+  // 테스트를 진행할 케이스
+  it("recoverToken", async () => {});
+  it("approveRouter", async () => {});
+  it("flashswap", async () => {});
+  it("fallback", async () => {});
+}) 
